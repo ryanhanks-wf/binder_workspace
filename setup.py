4995674ec970f433c66c8f35f34a729048ccd954
Ryan Hanks <ryan.hanks@workiva.com>
2016-06-06 11:38:25 -0700

    setup.py package name should be binders_workspace

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='bdub',
-    version='0.1',
+    name='binders_workspace',
+    version='0.0.1',
     py_modules=['bdub'],
     install_requires=[
         'Click',
